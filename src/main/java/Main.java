a174bf86345a843f1885842d2ff7c72430c513f8
Denis Lazykin <dlazykin856@gmail.com>
2020-12-22 23:07:29 +0300

    delete Customer class, corrected the name of classes and variables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,12 @@
 public class Main {
-
     public static void main(String[] args) throws InterruptedException {
+        Shop shop = new Shop();
 
-        final Autosalon autosalon = new Autosalon();
+        new Thread(shop::produceCar, "Производитель Toyota").start();
 
-        new Thread(null, autosalon::sellAuto, "Автосалон").start();
-
-        for (int i = 1; i < 11; i++) {
-            Thread.sleep(3000);
-            new Thread(null, autosalon::buyAuto, "Покупатель " + i).start();
+        for (int i = 0; i < 10; i++) {
+            Thread.sleep(2000);
+            new Thread(shop::buyCar, "Покупатель " + (i + 1)).start();
         }
     }
 }
